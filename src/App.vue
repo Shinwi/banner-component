871204e4529f7816f2c7a37ee503ac682e8f156a
Oualid Ezzoubeidi <siszko37@gmail.com>
2023-01-23 21:44:02 +0100

    init setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <banner-component
+      :mode="'square'"
+      :items="items"
+    ></banner-component>
+
+    <!-- <banner-component
+      :mode="'reCtangle'"
+      :carouselOnMobil="true"
+      :items="items"
+    ></banner-component> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BannerComponent from './components/BannerComponent.vue';
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    BannerComponent
+  },
+  data () {
+    return {
+      items: [
+        {
+          type: 'image',
+          aspectRatio: 'square',
+          src: 'https://picsum.photos/400',
+          link: 'https://example.com/link1'
+        },
+        {
+          type: 'cta',
+          title: 'CTA Title',
+          button: 'CTA Button',
+          link: 'https://example.com/link2'
+        },
+        {
+          type: 'image',
+          aspectRatio: 'square',
+          src: 'https://picsum.photos/400',
+          link: 'https://example.com/link3'
+        }
+      ]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/BannerComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>hello im component</h2>
+    <div>
+      <h2>mode : {{ mode }}</h2>
+    </div>
+    <div>
+      <h2>carous : {{ carouselOnMobil }}</h2>
+    </div>
+    <div>
+      <h2>items : {{ items }}</h2>
+    </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'BannerComponent',
+  props: {
+    mode: {
+      type: String,
+      required: true,
+      validator: function (value) {
+        return ['square', 'rectangle'].includes(value.toLowerCase())
+      }
+    },
+    items: {
+      type: Array,
+      required: true
+    },
+    carouselOnMobil: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
