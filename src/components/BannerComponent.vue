7685060d9896893a071f88bb4c5190a67f2f58f7
Oualid Ezzoubeidi <siszko37@gmail.com>
2023-01-25 18:07:06 +0100

    rectangle mode to work on phone screen

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -18,10 +18,10 @@
             <img :src="getRectangleModeImage.src" class="fit-image-in-container">
           </a>
         </b-col>
-        <b-col lg="4" id="align-items-cta">
+        <b-col lg="4" id="align-items-cta" :style="'background:' + (isMobile ? 'white;' : 'lightblue;')">
           <h3><strong>{{ getRectangleModeCTA.title }}</strong></h3>
           <a :href="getRectangleModeCTA.link" target="_blank" class="cta-button"><strong>{{ getRectangleModeCTA.button }}</strong></a>
-        </b-col>
+        </b-col> 
       </b-row>
       <b-row v-else>
         <h2 style="color:red">Invalid props</h2>
@@ -35,6 +35,7 @@ export default {
   data () {
     return {
       isImageLoaded: false,
+      windowWidth: window.innerWidth
     }
   },
   props: {
@@ -54,27 +55,28 @@ export default {
       default: false
     }
   },
-  /*
-    Since the background color of the cta div is random:
-    - we create an array of colors
-    - select a random color from it
-    - assign it to cta div when the page is rendered.
-    I only used 3 colors here (the one used in the design documents, but we can add more if we wish to.)
-  */
   mounted () {
-    // if (this.mode.toLowerCase() === 'square') {
-    //   var colors = ['lightblue', 'lightgreen', 'lightgray']
-    //   var random_color = colors[Math.floor(Math.random() * colors.length)]
-    //   document.getElementById('cta').style.background = random_color
-    // }
-    var colors = ['lightblue', 'lightgreen', 'lightgray']
-    var random_color = colors[Math.floor(Math.random() * colors.length)]
-    document.getElementById('align-items-cta').style.background = random_color
+    window.addEventListener('resize', () => {
+      this.windowWidth = window.innerWidth
+    })
+
+    /*
+      Since the background color of the cta div in desktop mode is random:
+      - we create an array of colors
+      - select a random color from it
+      - assign it to cta div when the page is rendered.
+      I only used 3 colors here (the one used in the design documents, but we can add more if we wish to.)
+      If we are in phone mode, background is set to white
+    */
+    if (!this.isMobile) {
+      var colors = ['lightblue', 'lightgreen', 'lightgray']
+      var random_color = colors[Math.floor(Math.random() * colors.length)]
+      document.getElementById('align-items-cta').style.background = random_color
+    }
   },
   computed: {
     isMobile () {
-      // TODO: update this
-      return false
+      return this.windowWidth <= 760
     },
     getRectangleModeImage () {
       return this.isMobile ? this.items.filter(i => i.type === 'image' && i.aspectRatio !== 'rectangle')[0] : this.items.filter(i => i.type === 'image' && i.aspectRatio !== 'square')[0]
